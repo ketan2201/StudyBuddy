4f03c7cc8c02a291a068408f39bb977a6399ecad
ketan2201 <139324520+ketan2201@users.noreply.github.com>
2024-06-25 20:38:16 +0530

    Initial commit

---
+++ Education-main/javascript/form.js
@@ -0,0 +1,42 @@
+//form loading animation
+
+const form = [...document.querySelector('.form').children];
+
+form.forEach((item, i) => {
+    setTimeout(() => {
+        item.style.opacity = 1;
+    }, i*100);
+})
+
+//form validation
+
+const name = document.querySelector('.name') || null;
+const email = document.querySelector('.email'); 
+const password = document.querySelector('.password'); 
+const submitBtn = document.querySelector('.submit-btn'); 
+
+if(name == null){//means login page is open
+
+} else{//means register page is open
+
+    submitBtn.addEventListener('click', () => {
+        fetch('/register-user', {
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                name: name.value,
+                email: email.value,
+                password: password.value
+            })
+        })
+        .then(req => res.json())
+        .then(data => {
+            if(data.name){
+                alert('register successful');
+            } else{
+                alert(data);
+            }
+        })
+    })
+
+}
